67d93eb5a031fbf4d6b0474b4b2a5821bf9def2e
P-h-1-L-z-A <kai.jaipur15@gmail.com>
2024-07-26 00:07:42 +0530

    step 1 : pushing code and opening Repo

---
+++ foodfront/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Say bye to your cravings</h2>
+            <p>Discover a delightful array of flavors with our fresh, innovative dishes, crafted to tantalize your taste buds and elevate your dining experience.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
