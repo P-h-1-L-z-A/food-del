d4f2cbeda87703c416ad2d548a05590bd5c62d5b
P-h-1-L-z-A <kai.jaipur15@gmail.com>
2024-07-28 02:17:22 +0530

    step 4

--- foodfront/src/components/Navbar/Navbar.jsx
+++ foodfront/src/components/Navbar/Navbar.jsx
@@ -44,10 +44,10 @@
 
 
 
-import React, { useContext, useState } from 'react'
+import React, { Profiler, useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({ setShowLogin }) => {
@@ -55,7 +55,15 @@ const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   const [theme, setTheme] = useState("light");
 
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount,token,setToken } = useContext(StoreContext);
+
+  const navigate = useNavigate();
+
+  const logout = ()=>{
+      localStorage.removeItem("token");
+      setToken("");
+      navigate("/");
+  }
 
   const toggleTheme = () => {
     if (theme === "light") {
@@ -78,16 +86,31 @@ const Navbar = ({ setShowLogin }) => {
         <a href='#footer' onClick={() => { setMenu("contact-us") }} className={menu === "contact-us" ? "active" : ""}>contact us</a>
       </ul>
       <div className='navbar-right'>
-        <button className= "icon" onClick={toggleTheme}>
-          {/* {theme === "light" ? "Dark " : "Light"} */}
+        <label className="toggle-button">
+          <input type="checkbox" onChange={toggleTheme} />
+          <span className="slider"></span>
+        </label>
+
+        {/* <button className= "icon" onClick={toggleTheme}>
+          
           {theme === "light" ? <img src={assets.sun} alt=""/> : <img src={assets.moon} alt=""/> }
-        </button>
+        </button> */}
+
         <Link to='/order'><img src={assets.search_icon} alt="" /></Link>
         <div className='navbar-search-icon'>
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
-        <button onClick={() => { setShowLogin(true) }}> Sign in </button>
+        {!token?<button onClick={() => { setShowLogin(true) }}> Sign in </button>:
+        <div className='navbar-profile'>
+          <img src={assets.profile_icon} alt="" />
+          <ul className='nav-profile-dropdown'>
+            <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+            <hr/>
+            <li onClick={logout} ><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+          </ul>
+        </div>}
+        
       </div>
     </div>
   )
