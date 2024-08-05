ba1dc84a367eebf7d952ec137e0b775192fc2935
P-h-1-L-z-A <kai.jaipur15@gmail.com>
2024-08-05 23:15:24 +0530

    step  7 the end

--- foodback/controllers/orderController.js
+++ foodback/controllers/orderController.js
@@ -6,7 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing user order for frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    // const frontend_url = "http://localhost:5173";
+    const frontend_url = "http://localhost:5174";
 
     try {
         const newOrder = new orderModel({
@@ -91,4 +92,27 @@ const userOrders = async (req,res)=>{
     }
 }
 
-export { placeOrder,verifyOrder,userOrders };
+//Listing orders for admin panel
+const listOrders = async (req,res)=>{
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})
+        
+    }
+}
+
+//api for updating orde status
+const updateStatus = async(req,res)=>{
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export { placeOrder,verifyOrder,userOrders,listOrders,updateStatus };
