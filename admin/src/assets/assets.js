6ae63065e950aa631ba2e52ee4e44a8b32563161
Bhavya Gupta <111755168+P-h-1-L-z-A@users.noreply.github.com>
2024-08-16 16:04:40 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,6 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+// export const currency = '₹'
+
+export const url = 'https://food-del-backend-jy0v.onrender.com'
